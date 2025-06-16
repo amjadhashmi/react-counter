95a6f7bdcab56f03a2d9142fef7cec61c521d944
Amjad-Hashmi <hashmiamjadpak@gmail.com>
2025-06-16 22:37:09 +0500

    remove un used code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor : {
+        amjadYello: "green"
+      },
+      fontFamily: {
+        giga: "Lexend Giga"
+      }
+    },
   },
   plugins: [],
 }
